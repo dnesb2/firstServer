7b887dd6564e359406ab9f9c80ecd8f497b9624f
dnesb2 <161069112+dnesb2@users.noreply.github.com>
2026-07-18 00:26:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ async def execute_script(request: Request):
     result = {"message": "Success", "received": data}
     
     return result
+
+# Change @app.post to @app.get
+@app.get("/execute2")
+async def execute_script():
+    # Note: GET requests usually do not have a JSON body
+    result = {"message": "Success! The GET request worked."}
+    return result
